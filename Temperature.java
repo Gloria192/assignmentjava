272173b2e60a2d0fc8a22427a1b6e0ff9e6e155e
Gloria <mutuyegloria@gmail.com>
2025-03-27 12:53:26 -0700

    first committ

---
+++ Temperature.java
@@ -0,0 +1,7 @@
+public class Temperature {
+    public static void main(String[] args) {
+        float celsius = 25.0f; 
+        double fahrenheit = (celsius * 9 / 5) + 32; 
+        System.out.println(fahrenheit); 
+    }
+}
